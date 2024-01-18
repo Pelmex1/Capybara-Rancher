ffbabb51f0a9750e253c924d259d808ac2076e16
Rob2043 <135964485+Rob2043@users.noreply.github.com>
2024-01-18 21:55:59 +0100

    add base script

--- Assets/Scripts/Main Menu.cs
+++ Assets/Scripts/Main Menu.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject PanelOptions;
+    [SerializeField] private GameObject PanelButton;
+    [SerializeField] private GameObject PanelMultiplayer;
+    [SerializeField] private GameObject PanelLoad;
+
+    public void PlayNewGame()
     {
-        
+        SceneManager.LoadScene("Level 1");
+    } 
+    public void PlayContinue()
+    {
+        SceneManager.LoadScene("Level 1");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnOptions()
     {
-        
+        PanelButton.SetActive(false);
+        PanelOptions.SetActive(true);
+    }
+
+    public void PlayMultiplayer()
+    {
+        PanelButton.SetActive(false);
+        PanelMultiplayer.SetActive(true);
+    }
+    public void PlayLoad()
+    {
+        PanelButton.SetActive(false);
+        PanelLoad.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
