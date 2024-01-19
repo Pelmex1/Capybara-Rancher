1ea5d173934db1743b48a3b4a8259c2c50cb6858
Pelmex1 <89773802+Pelmex1@users.noreply.github.com>
2024-01-19 17:33:36 +0200

    Moving

---
+++ Assets/Scripts/MobsAI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MobsAi : MonoBehaviour
+{
+    [SerializeField] private MobsControl mobsControl;
+    [SerializeField] private NavMeshAgent agent;
+    private void Start() {
+        StartCoroutine(Moving());
+    }
+    private Vector3 RandomPosition(){
+        Transform currentPoint = mobsControl.MobsLocations[Random.Range(0,mobsControl.MobsLocations.Length)];
+        return new Vector3(currentPoint.position.x / Random.Range(2,4),currentPoint.position.y,currentPoint.position.x / Random.Range(2,4));
+    }
+    private IEnumerator Moving(){
+        agent.destination = RandomPosition();
+        yield return new WaitForSecondsRealtime(30);
+        StartCoroutine(Moving());
+    }
+}
