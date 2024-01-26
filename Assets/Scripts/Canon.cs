909961d11876e81ab5569ebc95ac81c0ab6dfd39
Pelmex1 <89773802+Pelmex1@users.noreply.github.com>
2024-01-26 23:07:08 +0200

    fix

--- Assets/Scripts/Canon.cs
+++ Assets/Scripts/Canon.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class Canon : MonoBehaviour
 {
     [SerializeField] private GameObject canonEnter;
-    private int index = 0;
     private readonly float speed = 5f; 
     public List<Transform> objectsInCollider;
     private void OnTriggerEnter(Collider other) {
@@ -18,8 +17,14 @@ public class Canon : MonoBehaviour
         }
     }
     private void FixedUpdate() {
-        if(index == objectsInCollider.Count - 1) index = 0;
-        Vector3.Lerp(objectsInCollider[index].position, canonEnter.transform.position, speed * Time.fixedDeltaTime);
-        index++;
+        if(objectsInCollider.Count != 0){
+            if(Input.GetMouseButton(0)){
+                for(int i = 0; i < objectsInCollider.Count; i++){
+                    Debug.Log("sssss");
+                    //Vector3.Lerp(objectsInCollider[i].position, canonEnter.transform.position, speed * Time.deltaTime);
+                    Vector3.Lerp(canonEnter.transform.position,objectsInCollider[i].position, speed * Time.deltaTime);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/MovingPlayerCamera.cs
+++ Assets/Scripts/MovingPlayerCamera.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 
 public class MovingPlayer : MonoBehaviour
 {
-    [SerializeField] private  GameObject head;
+    [SerializeField] private Transform head;
+    [SerializeField] private Transform gun;
     [SerializeField] private float speed;
     private Rigidbody rb;
     private float vertical;
     private float horizontal;
-    private float xRotation = -90f;
+    private float xRotationCamera;
+    private float xRotationGun;
     public float mouseSensitivy;
+    private Quaternion startHeadRotation;
+    private Quaternion startGunRotation;
     private void Start() {
         rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
+        startHeadRotation = head.rotation;
+        startGunRotation = gun.rotation;
     }
     private void FixedUpdate() {
         
@@ -27,10 +33,14 @@ public class MovingPlayer : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivy * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivy * Time.deltaTime;
         
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -100f, 0f);
+        xRotationCamera -= mouseY;
+        xRotationCamera = Mathf.Clamp(xRotationCamera, startHeadRotation.y - 100f, startHeadRotation.z + 90f);
+        head.localRotation = Quaternion.Euler(xRotationCamera, 0f, 0f);
 
-        head.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        //xRotationGun -= mouseY;
+        //xRotationGun = Mathf.Clamp(xRotationGun, x, 0f);
+        //gun.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
         transform.Rotate(Vector3.up * mouseX);
     }
 }
