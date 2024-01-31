3942e2724547efbd631a49b37f5ef8120b1bad62
Pelmex1 <89773802+Pelmex1@users.noreply.github.com>
2024-01-31 10:11:38 +0200

    dsjds

--- Assets/Scripts/Canon.cs
+++ Assets/Scripts/Canon.cs
@@ -3,28 +3,17 @@ using UnityEngine;
 
 public class Canon : MonoBehaviour
 {
-    [SerializeField] private GameObject canonEnter;
     private readonly float speed = 5f; 
-    public List<Transform> objectsInCollider;
-    private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.CompareTag("movebleObject")){
-            objectsInCollider.Add(other.gameObject.transform);
-        }
-    }
-    private void OnTriggerExit(Collider other) {
-        if(other.gameObject.CompareTag("movebleObject")){
-            objectsInCollider.Remove(other.gameObject.transform);
-        }
-    }
-    private void FixedUpdate() {
-        if(objectsInCollider.Count != 0){
-            if(Input.GetMouseButton(0)){
-                for(int i = 0; i < objectsInCollider.Count; i++){
-                    Debug.Log("sssss");
-                    //Vector3.Lerp(objectsInCollider[i].position, canonEnter.transform.position, speed * Time.deltaTime);
-                    Vector3.Lerp(canonEnter.transform.position,objectsInCollider[i].position, speed * Time.deltaTime);
-                }
+    private void Update() {
+        //if(Input.GetMouseButton(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(ray.origin, ray.direction * 50f, Color.red);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            { 
+                Debug.Log(hit.transform.gameObject.name);
             }
-        }
+        //}
     }
 }
--- Assets/Scripts/CanonCollider.cs
+++ Assets/Scripts/CanonCollider.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class CanonCollider : MonoBehaviour
 {
-    [SerializeField] private Canon canonScript;
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("movebleObject")){
             // In inventory function here
-            canonScript.objectsInCollider.Remove(other.gameObject.transform);
             Destroy(other.gameObject);
         };
     }
