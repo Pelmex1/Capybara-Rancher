d76802fb304c2a6b79de4952300ed13f95758c05
Yarikus <yarikusens@gmail.com>
2024-02-08 22:33:26 +0200

    WallsForCattlePen Fix

--- Assets/Scripts/WallScript.cs
+++ Assets/Scripts/WallScript.cs
@@ -4,23 +4,12 @@ public class WallScript : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision!");
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Collision Player!");
             GetComponent<Collider>().isTrigger = true;
         }
         else
         {
-            Debug.Log("Collision Capybara!");
-            GetComponent<Collider>().isTrigger = false;
-        }
-    }
-    private void OnTriggerEnter(Collider collider)
-    {
-        if (collider.gameObject.CompareTag("Capybara"))
-        {
-            Debug.Log("Collider Capybara!");
             GetComponent<Collider>().isTrigger = false;
         }
     }
