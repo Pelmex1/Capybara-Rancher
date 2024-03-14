42607152b079332a37aeb488e81ffc551323ad54
Pelmex1 <89773802+Pelmex1@users.noreply.github.com>
2024-03-14 23:00:45 +0200

    AddChestFunc

--- Assets/Scripts/MapScripts/ChestsController.cs
+++ Assets/Scripts/MapScripts/ChestsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class ChestsController : MonoBehaviour
 {
     [SerializeField] private GameObject InventoryPanel;
     [SerializeField] private GameObject ChestPanel;
+    [SerializeField] private Transform CellsCanvas;
     private ChestCell[] chestsUI;
     private ChestCell[] inventoryUI;
 
@@ -14,23 +16,48 @@ public class ChestsController : MonoBehaviour
         inventoryUI = InventoryPanel.GetComponentsInChildren<ChestCell>();
         chestsUI = ChestPanel.GetComponentsInChildren<ChestCell>();
     }
-    public void UpdateChestCells(ChestCell[] chestCells){
+    private void UpdateChestCells(ChestCell[] chestCells){
         for(int i = 0 ; i < chestCells.Length; i++){
             chestsUI[i] = chestCells[i];
         }
     }
-        public void UpdateInventoryCells(ChestCell[] inventoryCells){
+    private void UpdateInventoryCells(ChestCell[] inventoryCells){
         for(int i = 0 ; i < inventoryCells.Length; i++)
         {
             inventoryUI[i] = inventoryCells[i];
         }
     }
+    private ChestCell FoundPos(Vector3 positionNow, ChestCell chestCell){
+        float pos = 0;
+        for(int i = 0; i < chestsUI.Length; i++){
+            float tmp2 = Math.Abs(Vector3.Distance(chestsUI[i].gameObject.transform.position, positionNow));     
+            if(tmp2 > pos){
+                pos = tmp2;
+                chestCell = inventoryUI[i];
+            }
+        }
+        for(int j = 0; j < inventoryUI.Length; j++){
+            float tmp = Math.Abs(Vector3.Distance(inventoryUI[j].gameObject.transform.position, positionNow));
+            if(tmp > pos){
+                pos = tmp;
+                chestCell = inventoryUI[j];
+            }
+        }
+        return chestCell;
+    }
+    private void SetParentObject(Transform transformGameObject){
+        transformGameObject.SetParent(CellsCanvas);
+    }
     private void OnEnable() 
     {
+        ChestCell.SetCanvasParent += SetParentObject;
+        ChestCell.FoundDistance += FoundPos;
         ContainerInventory.UpdateChestUI += UpdateChestCells;
         ContainerInventory.UpdateInventoryUI += UpdateInventoryCells;
     }
     private void OnDisable() {
+        ChestCell.SetCanvasParent -= SetParentObject;
+        ChestCell.FoundDistance -= FoundPos;
         ContainerInventory.UpdateChestUI -= UpdateChestCells;
         ContainerInventory.UpdateInventoryUI -= UpdateInventoryCells;
     }
--- Assets/Scripts/Scriptical Object/ChestCell.cs
+++ Assets/Scripts/Scriptical Object/ChestCell.cs
@@ -1,11 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ChestCell : MonoBehaviour
+public class ChestCell : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     public Image image;   
     public InventoryItem inventoryItem;
     public int count;
+    public delegate void AddDragEvent(Transform transform);
+    public static event AddDragEvent SetCanvasParent;
+    public delegate ChestCell AddCell(Vector3 position, ChestCell chestCell);
+    public static event AddCell FoundDistance;
+    private Vector3 pos;
+    private Transform parentTransform;
+    private ChestCell newChestCell;
+    private void Start() {
+        pos = transform.position;
+        parentTransform = transform.parent;
+    }
+    public void OnDrag(PointerEventData pointerEventData){
+        transform.position = pointerEventData.pointerCurrentRaycast.screenPosition;
+    }
+    public void OnBeginDrag(PointerEventData pointerEventData){
+        SetCanvasParent.Invoke(transform);
+    }
+    public void OnEndDrag(PointerEventData pointerEventData){
+        newChestCell = FoundDistance.Invoke(transform.position, newChestCell);
+        if(newChestCell.inventoryItem == null){
+            SetCellsData(ref newChestCell, inventoryItem, count);
+            inventoryItem = null;
+            count = 0;
+            transform.position = pos;
+            transform.SetParent(parentTransform);
+        } else {
+            InventoryItem inventoryItemTMP = newChestCell.inventoryItem;
+            int countTMP = newChestCell.count;
+            SetCellsData(ref newChestCell, inventoryItem, count);
+            inventoryItem = inventoryItemTMP;
+            count = countTMP;            
+            transform.position = pos;
+            transform.SetParent(parentTransform);
+        }
+    }
+    private void SetCellsData(ref ChestCell chestCell, InventoryItem inventoryItem, int count){
+        chestCell.inventoryItem = inventoryItem;
+        chestCell.count = count;
+    }
 }
