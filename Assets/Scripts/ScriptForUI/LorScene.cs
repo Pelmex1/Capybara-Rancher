7637a82f3cbe0c365430fc811b11abdfaac1c2a6
Rob2043 <135964485+Rob2043@users.noreply.github.com>
2024-03-24 17:50:46 +0100

    add Text for Load Scene

--- Assets/Scripts/ScriptForUI/LorScene.cs
+++ Assets/Scripts/ScriptForUI/LorScene.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections;
 using DevionGames;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class LorScene : MonoBehaviour
 {
+    [HeaderLine("Panels Objects")]
     [SerializeField] private GameObject LoadPanel;
     [SerializeField] private Image FirstImage;
     [SerializeField] private Image SecondImage;
+    [SerializeField] private TMP_Text text;
     [SerializeField] private Sprite[] LoadImages = new Sprite[3];
 
     [HeaderLine("Alfa")]
@@ -17,6 +20,7 @@ public class LorScene : MonoBehaviour
     public int CountImage = 0;
     private float StartAlfa;
     private float StartAlfa2;
+    private string AllText;
 
     public void OnLorScene()
     {
@@ -38,8 +42,9 @@ public class LorScene : MonoBehaviour
                 SecondImage.sprite = LoadImages[TemporaryInt];
                 IsChangeSecondImage = true;
             }
-            // WriteText
-            yield return new WaitForSeconds(5);
+            StartCoroutine(WriteText(CountImage));
+            yield return new WaitForSeconds(10);
+            text.text = "";
             for (float t = 0f; t < fadeDuration; t += Time.deltaTime)
             {
                 Color currentColor = FirstImage.color;
@@ -63,6 +68,33 @@ public class LorScene : MonoBehaviour
         else
         {
             SceneManager.LoadScene("Map");
+        }
+    }
+
+
+    private IEnumerator WriteText(int NumberImage)
+    {
+        text.text = "";
+        switch (NumberImage)
+        {
+            case 0:
+                AllText = @"In the distant past, colonists from another galaxy arrived on the planet Capybara tens of thousands of years ago. 
+                They brought capybaras with them, believing that these cute creatures would make good companions in their new environment. 
+                However, as a result of unknown events, the colonists disappeared and the capybaras remained on the planet and flourished in their development";
+                break;
+            case 1:
+                AllText = @"Many millennia later, our civilisation has detected signals from the system in which Capybara is located from afar. 
+                Fascinated by uncharted worlds and life forms, we sent a robotic explorer to the planet to study the Capybara and their fascinating world.";
+                break;
+            case 2:
+                AllText = @"However, after some time, communication with the robot broke down. What happened to it became a mystery to our civilisation.
+                 They decided to send a special agent you to the planet to solve the mystery and find out what happened to their robot";
+                break;
+        }
+        for (int i = 0; i <= AllText.Length; i++)
+        {
+            text.text += AllText[i];
+            yield return new WaitForSecondsRealtime(0.02f);
         }
     }
 }
